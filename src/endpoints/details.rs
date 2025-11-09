6a66bcf42f0a5a1bd767216e5894fa33b355c1d9
Dominic <141406838+dleutenegger@users.noreply.github.com>
2025-11-09 15:25:29 +0100

    feat: add list images endpoint

--- src/endpoints/details.rs
+++ src/endpoints/details.rs
@@ -56,6 +56,49 @@ impl PicsumClient {
             Err(err) => Err(UnexpectedError(err.to_string())),
         }
     }
+
+    /// Retrieve a list of available images.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use picsum_rs::endpoints::ImageSettings;
+    /// use picsum_rs::PicsumClient;
+    ///
+    /// // Retrieve a list of images, fetching page 1 with a limit of 10 images per page.
+    /// PicsumClient::default()
+    ///     .get_images(1, 10);
+    ///
+    /// ```
+    pub async fn get_images(
+        &self,
+        page: u16,
+        limit: u8,
+    ) -> Result<Vec<ImageDetails>, RequestError> {
+        let response = self
+            .inner
+            .client
+            .get(format!("{}/v2/list", BASE_URL))
+            .query(&vec![("page", page), ("limit", limit as u16)])
+            .send()
+            .await;
+
+        match response {
+            Ok(r) => match r.error_for_status() {
+                Ok(res) => res
+                    .json::<Vec<ImageDetails>>()
+                    .await
+                    .map_err(|err| InvalidResponse(err.to_string())),
+                Err(err) => match err.status() {
+                    Some(StatusCode::BAD_REQUEST) => Err(InvalidRequest(err.to_string())),
+                    Some(StatusCode::INTERNAL_SERVER_ERROR) => Err(ServerError(err.to_string())),
+                    Some(code) => Err(UnexpectedError(format!("{} {}", code, err))),
+                    None => Err(UnexpectedError(err.to_string())),
+                },
+            },
+            Err(err) => Err(UnexpectedError(err.to_string())),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -83,5 +126,21 @@ mod tests {
             download_url: "https://picsum.photos/id/1/5000/3333".to_string(),
         };
         assert_eq!(expected_details, details);
+    }
+
+    #[tokio::test]
+    async fn test_retrieve_images() {
+        let client = PicsumClient::default();
+
+        let response = client.get_images(1, 10).await;
+
+        assert!(
+            response.is_ok(),
+            "Retrieving page one with a limit of 10 images per page failed: {}",
+            response.unwrap_err().to_string()
+        );
+        let page1 = response.unwrap();
+
+        assert_eq!(10, page1.len());
     }
 }
